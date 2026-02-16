51d552d7df532d0fcdfb895d107053fb2884f7c1
Vasilisa321 <wolpina2006@gmail.com>
2026-02-16 15:47:36 +0700

    pr3
    Signed-off-by: Vasilisa321 <wolpina2006@gmail.com>

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ let app = new Vue ({
 
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 1,
+        onSale: true
     }
 })
